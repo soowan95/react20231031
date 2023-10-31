46c78227b160886d387a4d0b38f602b0ec18dc65
soowan95 <ksw9565@gmail.com>
2023-10-31 12:14:53 +0900

    practice component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      <MyComponent2 />
     </div>
   );
+}
+
+function MyComponent1 () {
+  return (
+    <div>
+      <h1>Hello Componenet</h1>
+    </div>
+  );
+}
+
+function MyComponent2 () {
+  return (
+    <>
+      <p>Lorem ipsum dolor sit.</p>
+      <ul>
+        <li>Lorem.</li>
+        <li>Sequi!</li>
+        <li>Nesciunt.</li>
+      </ul>
+    </>
+  )
 }
 
 export default App;
