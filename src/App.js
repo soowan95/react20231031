b77cbf802fcef548ec9b9ab3b8e2f31586e68fca
soowan95 <ksw9565@gmail.com>
2023-10-31 14:42:31 +0900

    learn how use js in jsx

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+function MyComp2() {
+  let name = "손흥민";
+  let age = 33;
+
+  // JSX에서 js코드 작성하기 위한 코드
+  return (
+    <>
+      <h1>hello react</h1>
+      <h1>hello {name}</h1>
+      <h1>{age} years</h1>
+      <h1>{age * 2} double years</h1>
+      <h1>{name + "player"}</h1>
+    </>
+  );
+}
+
 function App() {
   // jsx에서 태그 사용시 반드시 종료태그를 작성해야 함
   // 빈 요소이면 시작태그에서 종료해야 함
@@ -10,6 +26,7 @@ function App() {
         <br /> Lorem ipsum dolor.
       </p>
       <MyComp />
+      <MyComp2 />
     </div>
   );
 }
