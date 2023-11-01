f3d6e5437fc3f768965934724f16172aaacde5a1
soowan95 <ksw9565@gmail.com>
2023-11-01 11:47:24 +0900

    learn import & export

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
+// export된 컴포넌트를 import 해서 사용
+import { MyElem } from "./component/MyElem";
+import { MyComp } from "./component/MyComp";
+
 function App() {
   return (
     <>
-      <MyComp name="흥민" address="신촌" age={33} city="서울" />
-      <MyComp address="강남" age={22} city="부산" />
+      <MyComp />
+      <MyElem />
     </>
-  );
-}
-
-function MyComp({ name = "강인", address, city, age }) {
-  return (
-    <div>
-      <p>
-        {name}은 {age}세 이고 {address}, {city}에 산다
-      </p>
-    </div>
   );
 }
 
