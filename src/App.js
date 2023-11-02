51f1ac5e9100b36fca8039f277551e1af83b9ce7
soowan95 <ksw9565@gmail.com>
2023-11-02 11:18:46 +0900

    learn chakra ui Flex

--- src/App.js
+++ src/App.js
@@ -1,106 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Box bg="red" border="1px solid purple">
-        Lorem ipsum.
-      </Box>
-      <Box bg="blue" borderTop="2px dotted yellow">
-        Ea, minima.
-      </Box>
-      <Box bg="orange" borderBottom="7px double brown">
-        Architecto, tempore?
-      </Box>
-      <Box borderX="10px solid" borderColor="gray.600">
-        Iste, voluptas.
-      </Box>
-      <Box borderY="10px dotted" borderColor="red.300">
-        Ab, sequi!
-      </Box>
-      <Box border="5px solid blue" borderRadius="5px">
-        Accusamus, iure.
-      </Box>
-      <Box border="5px solid yellow" borderRadius="20%">
-        Animi, at.
-      </Box>
-      <Box border="5px solid black" borderRadius="2">
-        Neque, unde?
-      </Box>
-      <Box border="5px solid blue" borderTopRightRadius="10">
-        Id, voluptatum?
-      </Box>
-      <Box border="5px solid green" borderBottomRightRadius="10">
-        Enim, expedita.
-      </Box>
-      <Box border="5px solid purple" borderBottomLeftRadius="10">
-        Quam, soluta?
-      </Box>
-      <Box border="5px solid red" borderTopRightRadius="10">
-        Distinctio, modi.
-      </Box>
-      <Box border="5px solid skyblue" borderTopLeftRadius="10">
-        A, hic.
-      </Box>
-      <Box shadow="10px 5px 5px gray">Consequuntur, possimus?</Box>
-      <Box shadow="lg">Eveniet, veritatis!</Box>
-      <Box shadow="xl">Dignissimos, qui!</Box>
-      <Box shadow="md">Architecto, doloremque!</Box>
-      <Box bg="blue.300" m="10px">
-        Inventore, reprehenderit.
-      </Box>
-      <Box bg="yellow.600" m="20px">
-        Nobis, qui?
-      </Box>
-      <Box bg="facebook.500" mx="70px">
-        Harum, odio!
-      </Box>
-      <Box bg="red.300" my="50px">
-        Lorem ipsum dolor.
-      </Box>
-      <Box bg="red.600" m="1">
-        Lorem ipsum.
-      </Box>
-      <Box bg="blue.300" m="2">
-        Assumenda, eos.
-      </Box>
-      <Box bg="gold" m="3">
-        Possimus, vero!
-      </Box>
-      <Box bg="yellow.900" m="4">
-        Amet, delectus?
-      </Box>
-      <Box bg="gray.400" m="5">
-        Fugit, quibusdam.
-      </Box>
-      <Box bg="red.400" p="10px">
-        Lorem ipsum.
-      </Box>
-      <Box bg="blue.400" px="20px">
-        Obcaecati, similique.
-      </Box>
-      <Box bg="purple" py="30px">
-        Autem, possimus.
-      </Box>
-      <Box bg="gold" pl="20px">
-        Facere, quidem?
-      </Box>
-      <Box bg="tomato" p="1">
-        Illum, incidunt.
-      </Box>
-      <Box bg="red.600" p="2">
-        Iure, ut.
-      </Box>
-      <Box bg="blue.600" p="3">
-        Cumque, enim.
-      </Box>
-      <Box bg="black" pb="4">
-        Minus, quam?
-      </Box>
-      <Box bg="gray.300" pr="5">
-        Beatae, neque.
-      </Box>
-      <Box bg="skyblue">Id, provident.</Box>
+      <Flex
+        w="100%"
+        h="100px"
+        bg="gray.300"
+        gap="2"
+        align="center"
+        justify="space-around"
+      >
+        <Box bg="gold">Lorem.</Box>
+        <Box bg="red">Numquam.</Box>
+        <Box bg="gray">Illum?</Box>
+        <Box bg="blue">Facere!</Box>
+        <Box bg="green">Delectus!</Box>
+      </Flex>
     </>
   );
 }
