cc08b1443e69189b417b741aa7c0e43b847f6ea0
soowan95 <ksw9565@gmail.com>
2023-11-02 15:21:53 +0900

    learn chakra ui Textarea

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Box, FormControl, Select } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Select,
+  Textarea,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <>
       <Box w={"480px"}>
         <FormControl>
-          <Select placeholder="선택하세요.">
-            <option value="value1">Lorem.</option>
-            <option value="value2">Molestiae.</option>
-            <option value="value3">Sint.</option>
-            <option value="value4">Excepturi.</option>
-            <option value="value5">Commodi?</option>
-          </Select>
+          <FormLabel>본문</FormLabel>
+          <Textarea placeholder="본문을 작성하세요"></Textarea>
         </FormControl>
       </Box>
     </>
