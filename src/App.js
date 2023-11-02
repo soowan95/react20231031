faf78e432843a3455eb634b39346b9b709370a5e
soowan95 <ksw9565@gmail.com>
2023-11-02 16:30:52 +0900

    learn chakra ui Menu & practice

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
 import {
   Box,
+  Button,
   Flex,
-  FormControl,
-  FormLabel,
-  Image,
-  Select,
-  Switch,
-  Textarea,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
 } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
-    <>
-      <Box w={"480px"}>
-        <Image src="https://bit.ly/dan-abramov" />
-      </Box>
-      <Box>
-        <Image boxSize={"100px"} src="https://bit.ly/dan-abramov" />
-      </Box>
-      <Box>
-        <Image boxSize={"200px"} src="https://bit.ly/dan-abramov" />
-      </Box>
-      <Box>
-        <Image
-          boxSize={"200px"}
-          borderRadius={"full"}
-          src="https://bit.ly/dan-abramov"
-        />
-      </Box>
-      <Box>
-        <Image src="123123/123" fallbackSrc="https://via.placeholder.com/150" />
-      </Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-    </>
+    <Flex
+      px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
+      justifyContent={"space-between"}
+      py={"8px"}
+      my={"10px"}
+      bg={"gray.100"}
+    >
+      <Flex gap={"7px"} alignItems={"center"}>
+        <Text as={"b"}>Navbar</Text>
+        <Button variant={"ghost"}>Home</Button>
+        <Button variant={"ghost"}>Link</Button>
+
+        <Menu>
+          <MenuButton
+            variant={"ghost"}
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+          >
+            Dropdown
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+
+        <Button variant={"ghost"} isDisabled={true}>
+          Disabled
+        </Button>
+      </Flex>
+      <Flex gap={"7px"} alignItems={"center"}>
+        <Input size={"sm"} bg={"white"} colorScheme="green" />
+        <Button size={"sm"} colorScheme="green" variant={"outline"}>
+          Search
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
