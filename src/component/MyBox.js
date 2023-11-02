e2d0a825387fec4dd0b22250522d7be103eacaac
soowan95 <ksw9565@gmail.com>
2023-11-02 09:34:49 +0900

    delete no use file

--- src/component/MyBox.js
+++
@@ -1,9 +0,0 @@
-import style from "../css/style1.module.css";
-
-export function MyBox() {
-  return (
-    <div>
-      <h1 className={style.boxClass}>Lorem ipsum dolor sit amet.</h1>
-    </div>
-  );
-}
