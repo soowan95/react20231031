8978dc56e61833debbb91dd1ebffebaff8ad4958
soowan95 <ksw9565@gmail.com>
2023-11-02 14:11:35 +0900

    learn chakra ui Modal

--- src/App.js
+++ src/App.js
@@ -1,56 +1,46 @@
 import {
-  Box,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Flex,
-  Heading,
   Button,
-  SimpleGrid,
-  Table,
-  Thead,
-  Tbody,
-  Th,
-  Tr,
-  Td,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <SimpleGrid columns={1} gap={5}>
-        <Box>
-          <Box>
-            <Table variant="striped">
-              <Thead>
-                <Tr>
-                  <Th>Lorem.</Th>
-                  <Th>Saepe?</Th>
-                  <Th>At.</Th>
-                  <Th>Fugit.</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>Lorem ipsum dolor sit.</Td>
-                  <Td>Deleniti incidunt non tempore?</Td>
-                  <Td>Deserunt incidunt reiciendis suscipit.</Td>
-                  <Td>Aut ducimus eligendi sunt!</Td>
-                </Tr>
-              </Tbody>
-              <Tbody>
-                <Tr>
-                  <Td>Lorem ipsum dolor sit.</Td>
-                  <Td>Adipisci culpa iusto tempore!</Td>
-                  <Td>Debitis molestias tempore veritatis.</Td>
-                  <Td>At praesentium ullam veritatis?</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </Box>
-        </Box>
-      </SimpleGrid>
+      <Button onClick={onOpen}>모달 열기</Button>
+
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered={true}
+        motionPreset="slideInRight"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>모달의 제목</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+            nisi praesentium provident. Aut debitis explicabo laboriosam nam,
+            obcaecati quis! Accusantium asperiores aspernatur deleniti omnis
+            possimus quia rerum similique temporibus vitae!
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              닫기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
