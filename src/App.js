90078d5be88d44a759edd20f9134ba50d59dbf96
soowan95 <ksw9565@gmail.com>
2023-11-02 14:53:44 +0900

    learn chakra ui FormControl(FormLabel, Radio, Checkbox)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
-import { Button } from "@chakra-ui/react";
-import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Center,
+  Checkbox,
+  CheckboxGroup,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Lorem.</Button>
-      <Button colorScheme="red">Lorem.</Button>
-      <Button colorScheme="yellow">Provident?</Button>
-      <Button colorScheme="orange">Optio.</Button>
-      <Button colorScheme="teal" size={"sm"}>
-        Ipsum.
-      </Button>
-      <Button colorScheme="messenger" size={"lg"}>
-        Excepturi.
-      </Button>
-      <Button colorScheme="telegram" variant="outline">
-        Unde.
-      </Button>
-      <Button colorScheme="orange" variant="ghost">
-        Debitis!
-      </Button>
-      <Button variant="link">Aliquid.</Button>
-      <Button leftIcon={<EmailIcon />}>Aut.</Button>
-      <Button rigthIcon={<ArrowRightIcon />}>Vel.</Button>
-      <Button isLoading={true}>Dolores.</Button>
-      <Button isLoading loadingText="전송중">
-        Nisi.
-      </Button>
+      <Center>
+        <Box maxW={"800px"} minW={"400px"}>
+          <FormControl>
+            <FormLabel>이름</FormLabel>
+            <Input />
+            <FormHelperText>띄어쓰기 없이 입력하세요</FormHelperText>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center mt={5}>
+        <Box w={"480px"}>
+          <FormControl>
+            <FormLabel>하나만 선택하세요.</FormLabel>
+            <RadioGroup>
+              <Flex gap={5}>
+                <Radio value="value1">Lorem.</Radio>
+                <Radio value="value2">Lorem.</Radio>
+                <Radio value="value3">Deleniti.</Radio>
+                <Radio value="value4">Incidunt!</Radio>
+              </Flex>
+            </RadioGroup>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center>
+        <Box w={"480px"}>
+          <FormControl>
+            <FormLabel>체크박스</FormLabel>
+            <CheckboxGroup>
+              <Flex gap={5}>
+                <Checkbox colorScheme="orange">Lorem.</Checkbox>
+                <Checkbox colorScheme="green">Laudantium!</Checkbox>
+                <Checkbox colorScheme="purple">Assumenda!</Checkbox>
+              </Flex>
+            </CheckboxGroup>
+          </FormControl>
+        </Box>
+      </Center>
     </>
   );
 }
