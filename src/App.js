d9beeafe41a7183f182abf1f905afa4efae2e11f
soowan95 <ksw9565@gmail.com>
2023-11-02 15:17:29 +0900

    learn chakra ui Select

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import {
-  Box,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
+import { Box, FormControl, Select } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Center>
-        <Box maxW={"800px"} minW={"400px"}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>띄어쓰기 없이 입력하세요</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt={5}>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>하나만 선택하세요.</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Lorem.</Radio>
-                <Radio value="value3">Deleniti.</Radio>
-                <Radio value="value4">Incidunt!</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>체크박스</FormLabel>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox colorScheme="orange">Lorem.</Checkbox>
-                <Checkbox colorScheme="green">Laudantium!</Checkbox>
-                <Checkbox colorScheme="purple">Assumenda!</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w={"480px"}>
+        <FormControl>
+          <Select placeholder="선택하세요.">
+            <option value="value1">Lorem.</option>
+            <option value="value2">Molestiae.</option>
+            <option value="value3">Sint.</option>
+            <option value="value4">Excepturi.</option>
+            <option value="value5">Commodi?</option>
+          </Select>
+        </FormControl>
+      </Box>
     </>
   );
 }
