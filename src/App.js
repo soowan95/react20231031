c173a28a62b374d31320c7194ce658e64a30e917
soowan95 <ksw9565@gmail.com>
2023-11-03 16:24:23 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import { Box, Button, Center, Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 function App() {
-  let [number, setNumber] = useState(0);
-
-  function handlePlusButtonClick(e) {
-    setNumber(number + 1);
-  }
-  function handleMinusButtonClick(e) {
-    setNumber(number - 1);
+  const [message, setMessage] = useState("");
+  function handleChange(e) {
+    setMessage(e.target.value);
   }
   return (
     <>
-      <input type="number" value={number} />
-      <Box>
-        <Button onClick={handlePlusButtonClick}>+</Button>
-        <Button onClick={handleMinusButtonClick}>-</Button>
-      </Box>
-      <Box>
-        <Button onClick={() => setNumber(++number)}>+</Button>
-        <Button onClick={() => setNumber(--number)}>-</Button>
-      </Box>
+      <Input type="text" value={message} onChange={handleChange} />
     </>
   );
 }
