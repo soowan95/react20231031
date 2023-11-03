7a5bc071685e128e4fee86d19e7cf6eb16bbe4f6
soowan95 <ksw9565@gmail.com>
2023-11-03 12:23:38 +0900

    learn event.preventDefault()

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Input } from "@chakra-ui/react";
-
 function App() {
-  function handleChange(e) {
-    // console.log("input value is changed");
-    console.log(e.target.value);
+  function handleClick(e) {
+    // 브라우저가 해야하는 기본 기능을 취소함
+    e.preventDefault();
+    console.log("let do other work");
   }
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("let do other work");
+  }
   return (
     <>
-      <Input onChange={handleChange} />
-      <Input onChange={handleChange} />
+      <a href="https://www.naver.com" onClick={handleClick}>
+        naver
+      </a>
+      <div>
+        <form action="https://search.daum.net/search" onSubmit={handleSubmit}>
+          <input type="text" name="q" />
+          <button>검색</button>
+        </form>
+      </div>
     </>
   );
 }
