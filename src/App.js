69cd53273ddc4bf8bdfade7724ee7ce905ea6113
soowan95 <ksw9565@gmail.com>
2023-11-03 15:27:04 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Box, Button, Center, Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [number, setNumber] = useState(0);
 
-  function handleChange(e) {
-    setMessage(e.target.value);
+  const isOdd = number % 2 == 1;
+
+  function handleInputChange(e) {
+    setNumber(Number(e.target.value));
   }
 
   return (
     <>
-      <Input onChange={handleChange} />
-      <Text>{message}</Text>
+      <Input onChange={handleInputChange} type="number" />
+      <Text>{(isOdd && "홀수") || "짝수"}</Text>
     </>
   );
 }
