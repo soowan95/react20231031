27d7b00f3c6a0de07f4be3382dcd07642618e2e0
soowan95 <ksw9565@gmail.com>
2023-11-03 09:18:06 +0900

    learn children property

--- src/App.js
+++ src/App.js
@@ -1,58 +1,20 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+function MyComp({ text, address, children }) {
+  return (
+    <>
+      <p>{text}</p>
+      <p>{address}</p>
+      <p>{children}</p>
+    </>
+  );
+}
 
 function App() {
   return (
-    <Flex
-      px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
-      justifyContent={"space-between"}
-      py={"8px"}
-      my={"10px"}
-      bg={"gray.100"}
-    >
-      <Flex gap={"7px"} alignItems={"center"}>
-        <Text as={"b"}>Navbar</Text>
-        <Button variant={"ghost"}>Home</Button>
-        <Button variant={"ghost"}>Link</Button>
-
-        <Menu>
-          <MenuButton
-            variant={"ghost"}
-            as={Button}
-            rightIcon={<ChevronDownIcon />}
-          >
-            Dropdown
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Button variant={"ghost"} isDisabled={true}>
-          Disabled
-        </Button>
-      </Flex>
-      <Flex gap={"7px"} alignItems={"center"}>
-        <Input size={"sm"} bg={"white"} colorScheme="green" />
-        <Button size={"sm"} colorScheme="green" variant={"outline"}>
-          Search
-        </Button>
-      </Flex>
-    </Flex>
+    <>
+      <MyComp text={"hello"} address={"seoul"}>
+        some content 다른 컴포넌트가 있을 수도 있음.
+      </MyComp>
+    </>
   );
 }
 
