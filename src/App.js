e01adb6a501c5f2f26bbcaefb2ac44a32c90012b
soowan95 <ksw9565@gmail.com>
2023-11-06 10:29:47 +0900

    learn the wrong way using HOOK

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
+import { useState } from "react";
+
 function App() {
-  // HOOK : use 로 시작하는 함수
-  // HOOK 은 컴포넌트 상단에 작성해야 함.
-  // 조건문이나, 반복문, 중첩된 함수안에 작성하면 안됨.
+  const [number, setNumber] = useState(0);
 
-  if (true) {
-    // HOOK 작정 불사
-  }
-
-  for (let i = 0; i < 3; i++) {
-    // HOOK 작성 불가
-  }
-
-  function handle() {
-    // HOOK 작성 불가
+  if (number > 5) {
+    const [text, setText] = useState("");
   }
 
   return <></>;
