53ab7a1239feadee4e586fecd0849fdb053c8c66
soowan95 <ksw9565@gmail.com>
2023-11-06 12:22:36 +0900

    learn key for react when use List

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ function App() {
   const arr = ["pizza", "donkatzu", "hamburger", "kimchi"];
   const arr2 = ["son", "lee", "kim"];
   const arr3 = ["latte", "espresso", "capuchino"];
+  const arr4 = [
+    { id: 0, name: "latte" },
+    { id: 1, name: "espresso" },
+    { id: 2, name: "capuchino" },
+  ];
 
-  const listItems = arr.map((item) => <ListItem>{item}</ListItem>);
+  const listItems = arr.map((item, index) => (
+    <ListItem key={index}>{item}</ListItem>
+  ));
 
   return (
     <>
       <List mb={4}>{listItems} </List>
       <List mb={4}>
-        {arr2.map((i) => (
-          <ListItem>{i}</ListItem>
+        {arr2.map((i, index) => (
+          <ListItem key={index}>{i}</ListItem>
         ))}
       </List>
       <List>
-        {arr3.map((i) => (
-          <ListItem>{i}</ListItem>
+        {arr3.map((i, index) => (
+          <ListItem key={index}>{i}</ListItem>
         ))}
       </List>
     </>
