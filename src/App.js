a91199f00a35acf5e3a01480d520ae9b64c718f3
soowan95 <ksw9565@gmail.com>
2023-11-06 10:36:31 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { useState } from "react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 function App() {
+  // set... 메소드로 상태 변경할 수 있음
+  // state 가 같은지 아닌지 판단해서 re-render 결정함
   const [number, setNumber] = useState(0);
+  const [numberObject, setNumberObject] = useState({ number: 0 });
 
-  if (number > 5) {
-    const [text, setText] = useState("");
-  }
-
-  return <></>;
+  return (
+    <>
+      <Box>
+        <Button onClick={() => setNumber(number + 1)}>number 변경</Button>
+        <Text>{number}</Text>
+      </Box>
+      <Box>
+        <Button
+          onClick={() => {
+            numberObject.number = numberObject.number + 1;
+            setNumberObject(numberObject);
+          }}
+        >
+          number object 변경
+        </Button>
+        <Text>{numberObject.number}</Text>
+      </Box>
+    </>
+  );
 }
 
 export default App;
