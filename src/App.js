dcb35e6d9862938f3a940336a850311a6ae74c01
soowan95 <ksw9565@gmail.com>
2023-11-06 16:14:31 +0900

    learn immer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import { Input, Text } from "@chakra-ui/react";
+import { useImmer } from "use-immer";
+
 function App(props) {
-  return <></>;
+  const [person, setPerson] = useState({ name: "son", city: "seoul" });
+  const [user, updateUser] = useImmer({ name: "lee", city: "jeju" });
+
+  function handleNameChange(e) {
+    const nextPerson = { ...person };
+    nextPerson.name = e.target.value;
+
+    setPerson(nextPerson);
+  }
+
+  function handleCityChange(e) {
+    const nextPerson = { ...person };
+    nextPerson.city = e.target.value;
+
+    setPerson(nextPerson);
+  }
+
+  function handleUserNameChange(e) {
+    updateUser((draft) => {
+      draft.name = e.target.value;
+    });
+  }
+
+  function handleUserCityChange(e) {
+    updateUser((draft) => {
+      draft.city = e.target.value;
+    });
+  }
+
+  return (
+    <>
+      <Input value={person.name} onChange={handleNameChange} />
+      <Input value={person.city} onChange={handleCityChange} />
+      <Text>
+        {person.name}은 {person.city}에 삽니다.
+      </Text>
+
+      <hr />
+
+      <Input value={user.name} onChange={handleUserNameChange} />
+      <Input value={user.city} onChange={handleUserCityChange} />
+      <Text>
+        {user.name} is live in {user.city}
+      </Text>
+    </>
+  );
 }
 
 export default App;
