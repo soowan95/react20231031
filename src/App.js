7bdc70c862ce22334530a97e9886cc5d46a83b25
soowan95 <ksw9565@gmail.com>
2023-11-07 15:48:38 +0900

    learn useEffect()

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import { useEffect, useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import axios from "axios";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-
-  // component 외부시스템과 작업할 때
-  // 주로 ajax로 데이터 가져올 때
-
-  // 첫번째 parameter : 실행할 함수
-  // 두번째 parameter : 첫번째 parameter를 실행 시키는 값, 빈 배열이면 초기 rendering 때만 실행됨
+  const [myState, setMyState] = useState("");
   useEffect(() => {
-    console.log("이펙트의 첫번째 파라미터 함수 실행됨");
+    axios
+      .get("/api/main1/sub1")
+      .then(({ data }) => setMyState(data))
+      .catch((e) => console.log(e));
   }, []);
-
-  return (
-    <>
-      <Button onClick={() => setNumber(number + 1)}>증가</Button>
-      <Text>{number}</Text>
-    </>
-  );
+  return <div>{myState}</div>;
 }
 
 export default App;
