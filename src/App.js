ac26536905965037e3d5d75a8024176849b6efec
soowan95 <ksw9565@gmail.com>
2023-11-07 15:13:50 +0900

    clean master branch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import { createContext, useState } from "react";
-import { Acomp } from "./Acomp";
-import { Bcomp } from "./Bcomp";
-
 function App(props) {
-  const [message, setMessage] = useState("init state message");
-
-  return (
-    <>
-      {/* context에 value 넣기 : Context.Provider value={value} */}
-      <MessageContext.Provider value={{ message, setMessage }}>
-        <Acomp />
-        <Bcomp />
-      </MessageContext.Provider>
-    </>
-  );
+  return <></>;
 }
-
-// 1. context 만들기 : createContext
-export const MessageContext = createContext(null);
 
 export default App;
--- src/Bcomp.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { Text } from "@chakra-ui/react";
-import { MessageContext } from "./App";
-
-export function Bcomp() {
-  // 3. context 사용하기 : useContext()
-  const m = useContext(MessageContext);
-  return <Text>{m.message}</Text>;
-}
