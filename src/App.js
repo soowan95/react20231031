736ee8001bd2ac662e9fce8b19b57a009df1c304
soowan95 <ksw9565@gmail.com>
2023-11-08 10:24:18 +0900

    learn useEffect's second parameter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import { Box, Select, Spinner, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { Button, Input, Text } from "@chakra-ui/react";
+import axios from "axios";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  const [text, setText] = useState("");
-
-  // 첫번째 파라미터(setup): The function with your Effect's logic.
-  // 두번째 파라미터: setup을 실행시키는 값의 나열(배열)
-  //                빈 배열이면 첫 rendering때만 실행됨
+  const [customerId, setCustomerId] = useState(0);
+  const [customerName, setCustomerName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    console.log("excute effect function");
-  }, [number, text]);
+    setIsLoading(true);
+    axios
+      .get("/api/main1/sub4?id=" + customerId)
+      .then(({ data }) => setCustomerName(data))
+      .catch((e) => setCustomerName(null))
+      .finally(() => setIsLoading(false));
+  }, [customerId]);
   return (
     <div>
-      <Button onClick={() => setNumber(number + 1)}>증가</Button>
-      <Text>{number}</Text>
-      <Input value={text} onChange={(e) => setText(e.target.value)} />
-      <Text>{text}</Text>
+      <Select
+        placeholder="고객 번호를 선택하세요"
+        onChange={(e) => setCustomerId(e.target.value)}
+      >
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </Select>
+      <Box>
+        {(isLoading && <Spinner />) ||
+          (customerName && <Text>고객 이름 : {customerName}</Text>) || (
+            <Text>조회한 고객이 없습니다. 다른 번호를 선택해주세요.</Text>
+          )}
+      </Box>
     </div>
   );
 }
