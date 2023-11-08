f9065eb526b960c57088131df2eb845716b412aa
soowan95 <ksw9565@gmail.com>
2023-11-08 09:29:18 +0900

    learn useEffect's second parameter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { Button, Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [myState, setMyState] = useState("");
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  // 첫번째 파라미터(setup): The function with your Effect's logic.
+  // 두번째 파라미터: setup을 실행시키는 값의 나열(배열)
+  //                빈 배열이면 첫 rendering때만 실행됨
   useEffect(() => {
-    axios
-      .get("/api/main1/sub1")
-      .then(({ data }) => setMyState(data))
-      .catch((e) => console.log(e));
-  }, []);
-  return <div>{myState}</div>;
+    console.log("excute effect function");
+  }, [number, text]);
+  return (
+    <div>
+      <Button onClick={() => setNumber(number + 1)}>증가</Button>
+      <Text>{number}</Text>
+      <Input value={text} onChange={(e) => setText(e.target.value)} />
+      <Text>{text}</Text>
+    </div>
+  );
 }
 
 export default App;
