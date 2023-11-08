4ee6cb21e1b5544d992acd2af9294d5de3b74d2f
soowan95 <ksw9565@gmail.com>
2023-11-08 11:43:49 +0900

    learn useEffect's second parameter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App(props) {
   const [customerId, setCustomerId] = useState(0);
   const [customerName, setCustomerName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [customerIdList, setCustomerIdList] = useState([]);
+  useEffect(() => {
+    axios.get("/api/main1/sub6").then(({ data }) => setCustomerIdList(data));
+  }, []);
   useEffect(() => {
     setIsLoading(true);
     axios
@@ -20,16 +24,9 @@ function App(props) {
         placeholder="고객 번호를 선택하세요"
         onChange={(e) => setCustomerId(e.target.value)}
       >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
+        {customerIdList.map((id) => (
+          <option value={id}>{id}</option>
+        ))}
       </Select>
       <Box>
         {(isLoading && <Spinner />) ||
